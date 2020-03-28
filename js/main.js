43dbd39dba36c67afa1470b052b760697a1b1cba
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-03-28 18:18:29 +0300

    dashboard

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 $(function(){
 
 
+$('.dashboard-item img').click(function(){	
+	$(this).parent().parent().hide();			
+	});
+
+$('.close').click(function(){	
+	$('.popup').hide();			
+	});
+$('.popup-button').click(function(){	
+	$('.popup').css('display', 'flex');			
+	});
+
 
 });
+
+//-----------start close popup---------
+jQuery(function($){
+  $(document).mouseup(function (e){ 
+    var div = $(".popup"); 
+    var divWrap = $(".popup-content");
+    if (!divWrap.is(e.target) 
+        && divWrap.has(e.target).length === 0) {
+      div.hide();
+
+    }
+  });
+});
+//-----------end close popup---------
